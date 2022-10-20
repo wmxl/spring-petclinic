07bb3e3ec179c7dab6e25b0beedc17b35526bb89
Tony <294036748@qq.com>
2022-10-20 15:40:06 +0800

    uncomment @RefreshScope

--- src/main/java/org/springframework/samples/petclinic/system/HelloController.java
+++ src/main/java/org/springframework/samples/petclinic/system/HelloController.java
@@ -21,7 +21,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RefreshScope
+@RefreshScope
 @RestController
 class HelloController {
 
